a20bbe19a155f611143ef864aca9fbb20b90f18a
Adrian Ałaszewski <wns2148810@interia.pl>
2020-10-21 12:10:00 +0200

    Scroll button and header animations

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,18 +3,63 @@ import '../style/Header.sass'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons"
 
-const Header = () => {
-    return (
-        <header>
-            <div className="name-container">
-                <h1>Adrian A<span className={"special-letter"} data-char="L">Ł</span>aszewski</h1>
-                <h3>Software Engineer</h3>
-            </div>
-            <div className="header-scroll">
-                <FontAwesomeIcon icon={faChevronDown} size="3x"/>
-            </div>
-        </header>
-    );
+type HeaderState = {
+    loaded: boolean
+    scrollButtonVisible: boolean
+};
+
+type HeaderProps = {};
+
+class Header extends React.Component<HeaderProps, HeaderState> {
+
+    state: HeaderState = {
+        loaded: false,
+        scrollButtonVisible: true
+    };
+
+    componentDidMount() {
+        this.setState({ loaded: true });
+        window.addEventListener('scroll', this.handleScroll, { passive: true });
+    }
+
+    handleScroll = () => {
+        const scrollPosition: number = window.pageYOffset
+        const visible: boolean = Header.isScrollButtonVisible(scrollPosition);
+        this.setScrollButtonVisible(visible)
+    }
+
+    setScrollButtonVisible = (isVisible: boolean) => {
+        this.setState({ scrollButtonVisible: isVisible })
+    }
+
+    private static isScrollButtonVisible(scrollY: number) {
+        return scrollY / window.innerHeight * 100 < 10;
+    }
+
+    render() {
+        const scrollButtonClasses =
+            `btn btn-invisible opacity-transition${this.state.scrollButtonVisible ? "" : " disabled"}`;
+
+        return (
+            <header>
+                <div className="name-container">
+                    <h1>Adrian A<span className={"special-letter"} data-char="L">Ł</span>aszewski</h1>
+                    <h3>Software Engineer</h3>
+                </div>
+                <a
+                    href={"#main"}
+                    className={scrollButtonClasses}
+                    style={{
+                        opacity: this.state.scrollButtonVisible ? 1 : 0
+                    }}
+                >
+                    <div className="header-scroll">
+                        <FontAwesomeIcon icon={faChevronDown} size="3x"/>
+                    </div>
+                </a>
+            </header>
+        );
+    }
 }
 
 export default Header;
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,7 +2,34 @@ import React from "react";
 
 const Main = () => {
     return (
-        <main>
+        <main id="main">
+            <p>Something will be here soon.</p>
+            <p>Something will be here soon.</p>
+            <p>Something will be here soon.</p>
+            <p>Something will be here soon.</p>
+            <p>Something will be here soon.</p>
+            <p>Something will be here soon.</p>
+            <p>Something will be here soon.</p>
+            <p>Something will be here soon.</p>
+            <p>Something will be here soon.</p>
+            <p>Something will be here soon.</p>
+            <p>Something will be here soon.</p>
+            <p>Something will be here soon.</p>
+            <p>Something will be here soon.</p>
+            <p>Something will be here soon.</p>
+            <p>Something will be here soon.</p>
+            <p>Something will be here soon.</p>
+            <p>Something will be here soon.</p>
+            <p>Something will be here soon.</p>
+            <p>Something will be here soon.</p>
+            <p>Something will be here soon.</p>
+            <p>Something will be here soon.</p>
+            <p>Something will be here soon.</p>
+            <p>Something will be here soon.</p>
+            <p>Something will be here soon.</p>
+            <p>Something will be here soon.</p>
+            <p>Something will be here soon.</p>
+            <p>Something will be here soon.</p>
             <p>Something will be here soon.</p>
             <p>Something will be here soon.</p>
             <p>Something will be here soon.</p>
